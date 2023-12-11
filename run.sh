7efa9efee8039e642548d2c60c24a199bf75bff4
guoxinyu0617 <130673910+guoxinyu0617@users.noreply.github.com>
2023-12-11 20:09:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #weibo
-python train.py --dataset weibo --dim 64 --lr 0.0006 --num-layers 3 --cuda 0 --margin 8.0 --c None --split-seed 13
+python train.py --dataset weibo --dim 64 --lr 0.0006 --num-layers 3 --cuda 0 --split-seed 13
 #twitter
-python train.py --dataset twitter --dim 16 --lr 0.0003 --num-layers 2 --cuda 0 --margin 5.0 --c None --split-seed 246813
+python train.py --dataset twitter --dim 16 --lr 0.0003 --num-layers 2 --cuda 0 --split-seed 246813
 # pheme
-python train.py --dataset pheme --dim 32 --lr 0.0003 --num-layers 3 --dropout 0.2 --cuda 0 --margin 1.0 --c None --split-seed 12345
+python train.py --dataset pheme --dim 32 --lr 0.0003 --num-layers 3 --dropout 0.2 --cuda 0 --split-seed 12345
